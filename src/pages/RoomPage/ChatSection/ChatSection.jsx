1ea0e1017d41a41d6315295cb27adbee200463cc
sakibsarker <sakib15-3824@diu.edu.bd>
2023-08-27 12:23:04 +0600

    dummy message

--- src/pages/RoomPage/ChatSection/ChatSection.jsx
+++ src/pages/RoomPage/ChatSection/ChatSection.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import ChatLabel from './ChatLabel'
+import Messages from './Messages'
 
 const ChatSection = () => {
   return (
-    <div className='chat_section_container'>ChatSection</div>
+    <div className='chat_section_container'>
+      <ChatLabel/>
+      <Messages/>
+    </div>
   )
 }
 
---
+++ src/pages/RoomPage/ChatSection/Messages.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const messages=[
+    {
+        content:'Hey',
+        identity:'Sakib',
+        messageCreatedByMe:true,
+    },
+    {
+        content:'Hello Sakib',
+        identity:'Sohel',
+        messageCreatedByMe:false,
+    },
+    {
+        content:'Hi Everyone',
+        identity:'Sakib',
+        messageCreatedByMe:true
+    },
+    {
+        content:'Hello Everyone',
+        identity:'Janntul Setu',
+        messageCreatedByMe:false,
+    },
+
+];
+
+const Message=({author,content,sameAuthor,messageCreatedByMe})=>{
+    const alignClass=messageCreatedByMe
+    ?"message_align_right":"message_align_left";
+
+    const authorText=messageCreatedByMe?'You':author;
+    const contentAdditionalStyles=messageCreatedByMe
+    ?"message_right_styles":"message_left_styles";
+
+    return(
+        <div className={`message_container ${alignClass}`}>
+            {!sameAuthor && <p className='message_title'>{authorText}</p>}
+            <p className={`message_content ${contentAdditionalStyles}`}>{content}</p>
+        </div>
+    )
+}
+
+const Messages = () => {
+  return <div className='messages_container'>
+    {messages.map((message,index)=>{
+        const sameAuthor=
+        index>0 && message.identity===messages[index-1].identity;
+
+        return(
+            <Message key={`${message.content}${index}`}
+            author={message.identity}
+            content={message.content}
+            sameAuthor={sameAuthor}
+            messageCreatedByMe={message.messageCreatedByMe}
+            />
+        )
+    })}
+    </div>
+}
+
+export default Messages
