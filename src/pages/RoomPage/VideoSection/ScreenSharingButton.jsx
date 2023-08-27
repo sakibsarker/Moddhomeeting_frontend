4a8118a941e25ecd9b2db375dc5ab12d596a47b5
sakibsarker <sakib15-3824@diu.edu.bd>
2023-08-27 11:33:16 +0600

    screen share

--- src/pages/RoomPage/VideoSection/ScreenSharingButton.jsx
+++ src/pages/RoomPage/VideoSection/ScreenSharingButton.jsx
@@ -1,15 +1,56 @@
 import React, { useState } from 'react'
 import SwitchImg from '../../../assets/switchToScreenSharing.svg'
+import LocalScreenSharingPreview from './LocalScreenSharingPreview'
+import * as webRTCHandler from '../../../utils/webRTCHandler'
 
+const constrains={
+  audio:false,
+  video:true,
+}
 
 const ScreenSharingButton = () => {
     const [isScreenSharingActive,setIsScreenSharingActive] =useState(false)
-    const handleScreenShareToggle=()=>{
-        setIsScreenSharingActive(!isScreenSharingActive);
+    const [screenSharingStream,setScreenSharingStream] =useState(null)
+
+
+    const handleScreenShareToggle=async()=>{
+      if(!isScreenSharingActive){
+        let stream=null;
+        try {
+          stream=await navigator.mediaDevices.getDisplayMedia(constrains);
+          
+        } catch (error) {
+          console.log('error occurred when trying ot get and access to screen share stream');
+          console.log(error);
+          
+        }
+        if(stream){
+          setScreenSharingStream(stream);
+          webRTCHandler.toggleScreenShare(isScreenSharingActive,stream);
+          setIsScreenSharingActive(true);
+          //execute here function to switch the video track which we are sending to other user
+        }
+      }
+      else{
+        webRTCHandler.toggleScreenShare(isScreenSharingActive);
+        //switch for video track from camera
+        setIsScreenSharingActive(false);
+
+        //stop screen share stram
+        // setScreenSharingStream.getTracks().forEach(t=>t.stop())
+
+        screenSharingStream.getTracks().forEach((track) => {
+          track.stop();
+        });
+        setScreenSharingStream(null);
+      }
+
+      // setIsScreenSharingActive(!isScreenSharingActive);
     }
 
 
   return (
+    <>
     <div className='video_button_container'>
         <img
         src={SwitchImg}
@@ -17,6 +58,10 @@ const ScreenSharingButton = () => {
         className='video_button_image'
         />
     </div>
+    {isScreenSharingActive && (
+      <LocalScreenSharingPreview stream={screenSharingStream}/>
+    )}
+    </>
   )
 }
 
--- src/utils/webRTCHandler.js
+++ src/utils/webRTCHandler.js
@@ -202,3 +202,36 @@ export const toggleMic=(isMuted)=>{
 export const toggleCamera=(isDisabled)=>{
     localStream.getVideoTracks()[0].enabled=isDisabled?true:false;
 }
+
+
+
+export const toggleScreenShare = (
+    isScreenSharingActive,
+    screenSharingStream = null
+  ) => {
+    if (isScreenSharingActive) {
+      switchVideoTracks(localStream);
+    } else {
+      switchVideoTracks(screenSharingStream);
+    }
+  };
+
+export const switchVideoTracks=(stream)=>{
+    for (let socket_id in peers) {
+        for (let index in peers[socket_id].streams[0].getTracks()) {
+          for (let index2 in stream.getTracks()) {
+            if (
+              peers[socket_id].streams[0].getTracks()[index].kind ===
+              stream.getTracks()[index2].kind
+            ) {
+              peers[socket_id].replaceTrack(
+                peers[socket_id].streams[0].getTracks()[index],
+                stream.getTracks()[index2],
+                peers[socket_id].streams[0]
+              );
+              break;
+            }
+          }
+        }
+      }
+}
